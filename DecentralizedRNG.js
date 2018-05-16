976319e7d9dab03a187f55a26e3e8460473cc481
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2018-05-15 20:33:44 -0700

    Updated about.

--- DecentralizedRNG.js
+++ DecentralizedRNG.js
@@ -8,10 +8,3 @@ var contract_address = "n1sJeUhCKE1r2hZnrbNAKtv5pTR2ydeWSsM";
 
 var NebPay = require("nebpay");
 var nebPay = new NebPay();
-
-//to check if the extension is installed
-//if the extension is installed, var "webExtensionWallet" will be injected in to web page
-if(typeof(webExtensionWallet) === "undefined"){
-    alert ("Extension wallet is not installed, you'll be redirected to the download instructions.");
-    window.location = "https://github.com/ChengOrangeJu/WebExtensionWallet";
-}
